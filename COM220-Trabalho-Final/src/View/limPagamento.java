2180b4cc77d788a7a0747fa3187b1fa3177106c9
Carlos-Henreis <carlos_henreis@unifei.edu.br>
2015-11-26 17:13:48 -0200

    Limite da classe paragamento adicionado

--- COM220-Trabalho-Final/src/View/limPagamento.java
+++ COM220-Trabalho-Final/src/View/limPagamento.java
@@ -2,8 +2,15 @@
 package View;
 
 import Model.*;
+import javax.swing.JOptionPane;
 
 
 public class limPagamento {
+    
+    public String montaForm() {
+        String aDadosForm;
+        aDadosForm = JOptionPane.showInputDialog("Valor");
+        return aDadosForm;
+    }
     
 }
