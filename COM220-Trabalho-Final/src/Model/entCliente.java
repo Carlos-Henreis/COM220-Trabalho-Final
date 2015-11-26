b96d96ccf8f6088d9e4d471adda7ffa283cc2dcd
Carlos-Henreis <carlos_henreis@unifei.edu.br>
2015-11-26 10:36:14 -0200

    Ajustes finais

--- COM220-Trabalho-Final/src/Model/entCliente.java
+++ COM220-Trabalho-Final/src/Model/entCliente.java
@@ -5,36 +5,46 @@ public class entCliente {
 
     
     //Declaração dos atributos
-     //Declaração dos atributos
+    int cpf;
     int telefone;
-    String endereco;
     String nome;
+    String endereco[];
     
     //construtor
-    public entCliente (int pTelefone, String pEndereco, String pNome){
+    public entCliente (int pTelefone, String[] pEndereco, String pNome, int pCpf){
        setTelefone (pTelefone);
        setEndereco (pEndereco);
        setNome (pNome);
+       setCpf (pCpf);
     }
     //gets e seters
     public void setTelefone (int pTelefone) {
         telefone = pTelefone;
     }
-    public void setEndereco (String pEndereco) {
+    public void setEndereco (String[] pEndereco) {
+        endereco = new String[4];//CEP, Cidade, Rua, Numero
         endereco = pEndereco;
     }
      public void setNome (String pNome) {
         nome = pNome;
     }
+    
+    public void setCpf (int pCpf){
+        cpf = pCpf;
+    }
      
     public int getTelefone () {
        return telefone;
     }
-    public String getEndereco () {
+    public String[] getEndereco () {
         return endereco;
     }
      public String getNome () {
          return nome ;
+    }
+     
+    public int getCpf (){
+        return cpf;
     }
     
 }
