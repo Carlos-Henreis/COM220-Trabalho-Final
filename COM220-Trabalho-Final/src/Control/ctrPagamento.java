3266a90aaf9686a2c8435d105c5d6737f8ce23d0
Carlos-Henreis <carlos_henreis@unifei.edu.br>
2015-11-26 17:10:14 -0200

    todos os metodos do controlador  da entidade Pagamento foram adicionados

--- COM220-Trabalho-Final/src/Control/ctrPagamento.java
+++ COM220-Trabalho-Final/src/Control/ctrPagamento.java
@@ -1,8 +1,33 @@
 
 package Control;
 
+import Model.*;
+import View.*;
+
 
 
 public class ctrPagamento {
+    
+    //Declaração dos atributos
+    private limPagamento objALimPagamento = new limPagamento();
+    private String aDadosForm;
+    //private Vector vecPagamentos = new Vector();
+    
+    public boolean registrarPagamento(entPagamento objAEntPagamento) throws Exception {
+        Registra();
+        atualizaSituacao(objAEntPagamento, Double.parseDouble(aDadosForm));
+        return true;
+    }
+    
+    private void Registra() {
+        aDadosForm = objALimPagamento.montaForm();
+    }
+    
+    private void atualizaSituacao(entPagamento objAEntPagamento, double pValor) throws Exception {
+        objAEntPagamento.setSitucao(pValor);
+        
+    }
+    
+    
     
 }
