ae536d430e982d60404739875d2eec152fabc5b5
Carlos-Henreis <carlos_henreis@unifei.edu.br>
2015-11-26 17:12:37 -0200

    Novos métodos da entidade pagamento foram atualizadas

--- COM220-Trabalho-Final/src/Model/entPagamento.java
+++ COM220-Trabalho-Final/src/Model/entPagamento.java
@@ -5,28 +5,36 @@ public class entPagamento {
     
     //Declaração dos atributos
     double valor;
-    String descricao;
+    double situacao;
     
     //construtor
-    public entPagamento (double pValor, String pDescricao){
+    public entPagamento (double pValor) throws Exception{
        setValor (pValor);
-       setDescricao (pDescricao);
+       setSitucao (0);
     }
+
+    public entPagamento() {
+        
+    }
+
     //gets e seters
     public void setValor (double pValor) {
         valor = pValor;
     }
 
-     public void setDescricao (String pDescricao) {
-         descricao = pDescricao;
+    public void setSitucao (double pValorP) throws Exception{
+        if (situacao+pValorP > 0) {
+            situacao = 0;
+            throw new Exception("Valor pago"+pValorP+"| Troco:"+(situacao+pValorP)+"\n");
+        }
+         situacao = situacao+pValorP;
     }
      
     public double getPreco () {
        return valor;
     }
-     public String getDescricao () {
-         return descricao ;
+    public double getSituacao () {
+         return situacao ;
     }
-   //outros metodos
     
 }
